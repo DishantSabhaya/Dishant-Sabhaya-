5745a07507e8a66b04bf5d32bf95a376ee9fe124
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-08-13 12:44:15 +0530

    Add files via upload

---
+++ c8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float i,d;
+    printf("\n convert to INR into doller");
+    printf("enter the inr:");
+    scanf("%f",&i);
+    d=i/48;
+    printf("\n your minutes is : %f",d);
+    getch();
+}
