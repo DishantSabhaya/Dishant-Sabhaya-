9e27a53812ac1ba82f4c793a2ee9ad4a38fd89cb
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-08-13 12:37:45 +0530

    Add files via upload

---
+++ c7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float h,m,a;
+    printf("\n convert to minutes into hours");
+    printf("enter the minutes:");
+    scanf("%f",&m);
+    h=m/60;
+    printf("\n your hours is : %f",h);
+    getch();
+}
+
