ea19b147ee14bcc0313b5e15e59e301ee0af29ad
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-08-20 12:04:13 +0530

    Add files via upload

---
+++ largest and smallest three number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b,c;
+printf("enter three number\n");
+scanf("%d%d%d",&a,&b,&c);
+{if(a>b)
+printf("%d is the largest number",a);
+else
+printf("%d is the largest number",b);
+}
+if(b>c)
+
+printf("%d is the larges number",b);
+else
+printf("%d is the largest number",c);
+
+}
